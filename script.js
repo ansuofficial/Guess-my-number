7ec4d8b02af5c79db41dac924d5628c08e032261
Ansuofficial <badjieansu165@gmail.com>
2023-09-18 13:13:50 +0000

    Second commit

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ let highscore=0;
 
 const message = getElement('.message')
 const number = getElement('.number')
+const _body = getElement('body')
+const guess = getElement('.guess')
 
 function getElement (element) {
     const item = document.querySelector(element)
@@ -22,17 +24,16 @@ function getElement (element) {
 }
 
 getElement('.check').addEventListener ('click', function () {
-    let x = Number((document.querySelector('.guess').value))
+    let x = Number((guess.value))
    
-
     if (!x) {
         message.textContent = 'There is no number'
-    } 
+    }
 
     else if (x === randomNumber) {
         number.textContent = randomNumber
         message.textContent = 'Congrats you got it right'
-        getElement('body').style.backgroundColor = 'green'
+        _body.style.backgroundColor = 'green'
         number.style.width = '30rem'
 
         getElement('.highscore').textContent = score > highscore? score: highscore;
@@ -53,25 +54,23 @@ getElement('.check').addEventListener ('click', function () {
 
     if (score < 0) {
         getElement('.score').textContent = 0
-        getElement('.message').textContent = 'You loose the game!!!'
-        getElement('body').style.backgroundColor = 'red'
+        message.textContent = 'You loose the game!!!'
+        _body.style.backgroundColor = 'red'
     }
 
 });
-
 
 getElement('.again').addEventListener('click', function () {
     randomNumber = Math.trunc(Math.random()*20+1)
     score=20;
 
-    getElement('body').style.backgroundColor ='#222';
+    _body.style.backgroundColor ='#222';
     getElement('.message').textContent = 'Start guessing...'
-    getElement('.guess').value = 0;
+    guess.value = 0;
     number.textContent = '?'
     number.style.width = '15rem'
     getElement('.score').textContent = score;
 });
-
 
 // const cameraFeed = document.getElementById('cameraFeed')
 
@@ -80,8 +79,3 @@ getElement('.again').addEventListener('click', function () {
 //     cameraFeed.srcObject = stream;
 // })
 
-const calcRandomNum = () => {
-    let calc = (70*50) / 12
-    return calc
-}
-console.log(calcRandomNum())
